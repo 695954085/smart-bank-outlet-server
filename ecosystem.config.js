0dba90ea49549472bbb0654ab0b1d89624b8dca6
695954085 <695954085@qq.com>
2018-11-12 15:01:06 +0800

    feat: 添加pm2生态文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,46 @@
 module.exports = {
-  apps: [{
-    name: 'email',
-    script: './email/index.js',
-    env: {
-      NODE_ENV: 'development',
+  apps: [
+    {
+      name: 'Email',
+      script: './email/index.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    }, {
+      name: 'Frontend',
+      script: './frontend/index.js',
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: 'production',
-    },
-  }, {
-    name: 'frontend',
-    script: './frontend/index.js',
-    env: {
-      NODE_ENV: 'development',
-    },
-    env_production: {
-      NODE_ENV: 'production',
-    },
-  }],
+  ],
 
   deploy: {
     production: {
-      user: 'root',
-      host: ['104.238.185.118'],
+      user: 'node',
+      host: '212.83.163.1',
       ref: 'origin/master',
-      repo: 'git@github.com:695954085/smart-bank-outlet-server.git',
-      path: '/root/workspace',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      ssh_options: 'StrictHostKeyChecking=no',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
     },
   },
 };
